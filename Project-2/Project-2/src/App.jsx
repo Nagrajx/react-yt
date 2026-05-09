210728d94d7c5a1bddd936ac18888bac8f5eeb5d
Nagrajx <satishnagraj54@gmail.com>
2026-05-09 21:47:22 +0530

    Practice

---
+++ Project-2/Project-2/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='satish'>Satish</h1>
+      <p className='l'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora provident consequuntur qui aspernatur laudantium quis, in hic dolorem ipsum rem numquam asperiores voluptatum assumenda, recusandae dolorum optio voluptatibus officia atque.</p>
+    </div>
+  )
+}
+
+export default App
